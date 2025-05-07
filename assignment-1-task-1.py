ef52a8de3ee512c94283f35155b9842cc96a6e54
sunit-bose <sunitbose109@gmail.com>
2025-05-07 13:17:47 +0530

    Add files via upload

---
+++ assignment-1-task-1.py
@@ -0,0 +1,35 @@
+'''
+s = "Hi eve! what string?"
+# Length should be 20
+print("Length of s = %d" % len(s))
+x = "Hello"
+print(x[::-1])
+'''
+
+#Assignment Task 1
+num1 = int(input("Enter the first number 1: "))
+num2 = int(input("Enter the second number 2: "))
+a = num1+num2
+s = num1 - num2
+m = num1*num2
+d = num1/num2
+
+print (" Addition: %d \n Substraction: %d \n Multiplication: %d \n Division: %d"  %(a,s,m,d) )
+
+
+
+#operator = (input("Enter operator (+,-,*,/): "))
+
+'''
+if operator == "+":
+    print (operand1+operand2)
+elif operator == "-":
+    print (operand1 - operand2)
+elif operator == "*":
+    print (operand1*operand2)
+elif operator == "/":
+    print (operand1/operand2)    
+'''
+
+
+print ("thanks") 
