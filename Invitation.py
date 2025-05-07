898fa75d7d937820e1d1980024db9c2a4795a4b3
sunit-bose <sunitbose109@gmail.com>
2025-05-07 13:33:04 +0530

    Update Invitation.py

--- Invitation.py
+++ Invitation.py
@@ -1,3 +1,7 @@
+#Print a invitation with first and last names as user inputs and then concat the inputs to print result in the following format
+  #>> Hello John doe!. Welcome to the python program.
+
+
 a = input("Enter your first name: ")
 b = input("Enter your last name: ")
 
